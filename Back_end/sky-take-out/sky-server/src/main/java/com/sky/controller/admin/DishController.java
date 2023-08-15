e062cb6fe84282dc50d0e9064bd31d4bd9cabda8
lingjunwan <118141976+lingjunwan@users.noreply.github.com>
2023-08-14 23:27:05 -0400

    TS-6:API-View-Dish

--- Back_end/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ Back_end/sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -6,16 +6,15 @@ package com.sky.controller.admin;
  */
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Dish Management
@@ -40,5 +39,19 @@ public class DishController {
         log.info("Create Dish：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
         return Result.success();
+    }
+
+    /**
+     * Dish Pagination Query
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("Dish Pagination Query")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO) {
+        log.info("Dish Pagination Query:{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 }
