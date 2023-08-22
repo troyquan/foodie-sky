90af86afff2f664c81889a8ae23a576aa4611fbe
vic <baotu120@gmail.com>
2023-08-22 19:17:35 -0400

    ts-23-client

--- Client_Console/src/Page/OrderHistory.tsx
+++ Client_Console/src/Page/OrderHistory.tsx
@@ -1,11 +1,12 @@
-import {useCancelOrderMutation, useGetOrderHistoryQuery} from "../Apis/orderApi.ts";
-import {useEffect, useState} from "react";
-import {Popconfirm, Space, Table} from 'antd';
-import {Link} from "react-router-dom";
-import {ColumnsType} from "antd/es/table";
+import {
+  useCancelOrderMutation,
+  useGetOrderHistoryQuery,
+} from "../Apis/orderApi.ts";
+import { useEffect, useState } from "react";
+import { Popconfirm, Space, Table } from "antd";
+import { Link } from "react-router-dom";
 
-
-const {Column} = Table;
+const { Column } = Table;
 
 interface DataType {
   id: number;
@@ -57,182 +58,111 @@ interface DataType {
   tablewareStatus: number;
 
   orderDishes: string | null;
-
 }
-
-
-
 
 const OrderHistory = () => {
   const [pagination, setPagination] = useState({
     current: 1,
-    pageSize: 5, // Default page size
+    pageSize: 20, // Default page size
   });
   const [cancelOrder] = useCancelOrderMutation();
-  const {data:orderdata,isLoading} = useGetOrderHistoryQuery({
+  const { data: orderdata, isLoading } = useGetOrderHistoryQuery({
     page: pagination.current,
-    pageSize:pagination.pageSize,
+    pageSize: pagination.pageSize,
   });
 
-  const [orderHistoryData,setOrderHistoryData] = useState([]);
+  const [orderHistoryData, setOrderHistoryData] = useState([]);
 
-  useEffect(()=>{
-    if (orderdata){
-      console.log("total: =======>",orderdata.apiResponse.data.total)
-      console.log("record: ------->",orderdata.apiResponse.data.record)
+  useEffect(() => {
+    if (orderdata) {
+      console.log("total: =======>", orderdata.apiResponse.data.total);
+      console.log("record: ------->", orderdata.apiResponse.data.records);
       settotalVal(Number(orderdata.apiResponse.data.total));
-      setOrderHistoryData(orderdata.apiResponse.data.record)
+      setOrderHistoryData(orderdata.apiResponse.data.records);
     }
-  },[orderdata,pagination])
+  }, [orderdata, pagination]);
 
-  const handleCancel = async (id) =>{
+  const handleCancel = async (id: number) => {
     try {
-      await cancelOrder({id});
-    } catch (err){
-      console.log("err: ",err)
-      return
+      await cancelOrder({ id });
+    } catch (err) {
+      console.log("err: ", err);
+      return;
     }
-  }
+  };
 
-  const [totalVal, settotalVal] = useState(0)
-  const statusTextMap:{[key:number]:string} = {
-    1:"Pending Payment",
-    2:"To Be Confirmed",
-    3:"Confirmed",
-    4:"Delivery",
-    5:"Completed",
-    6:"Cancelled"
-  }
+  const [totalVal, settotalVal] = useState(0);
+  const statusTextMap: { [key: number]: string } = {
+    1: "Pending Payment",
+    2: "To Be Confirmed",
+    3: "Confirmed",
+    4: "Delivery",
+    5: "Completed",
+    6: "Cancelled",
+  };
 
-
-  const columns: ColumnsType<DataType> = [
-    {
-      title:"Order Number" ,
-      dataIndex:"number" ,
-      key:"number"
-    },
-      {
-      title:"Status" ,
-      dataIndex:"status" ,
-      key:"status"
-    },
-      {
-      title:"OrderTime" ,
-      dataIndex:"orderTime" ,
-      key:"orderTime"
-    },
-      {
-      title:"Status" ,
-      dataIndex:"status" ,
-      key:"status",
-        render:(status: number)=>{
-        return <span>{statusTextMap[status]}</span>
-      }
-    },
-      {
-      title:"Price" ,
-      dataIndex:"price" ,
-      key:"price"
-    },
-      {
-      title:"Action" ,
-      key:"action",
-        render:(_: any, record:any)=>(
-            <Space size="middle">
-              <Link to={{
-                pathname: `/admin/category/${record.id}`,
-              }}
-              >Details</Link>
-
-              <Popconfirm
-                  title={ 'Are you sure cancel this order?'}
-                  okText="Yes"
-                  cancelText="No"
-                  onConfirm={()=>handleCancel(record.id)}
-              >
-                <a>{record.status === 2 ? 'Cancel' : ''}</a>
-              </Popconfirm>
-            </Space>
-        )
-    },
-
-  ]
-
-
-
-
-
-  const [pageVal, setpageVal] = useState(1)
-  const [pageSizeVal, setpageSizeVal] = useState(5)
-
+  const handleTableChange = (pagination: any) => {
+    setPagination(pagination);
+  };
   return (
-      <div>
+      <div className="min-h-[66vh]">
         {isLoading && (
             <div className="flex-1 flex justify-center items-center h-[50vh]">
               loading
             </div>
         )}
-        {!isLoading &&(<Table dataSource={orderHistoryData} rowKey={(record:DataType) => record.orderTime}
-                              pagination={
-                                {
-                                  defaultCurrent: 1,
-                                  total: totalVal,
-                                  showSizeChanger: true,
-                                  pageSize: pageSizeVal,
-                                  current: pageVal,
-                                  onChange: (page, pageSize) => {
-                                    setpageVal(page);
-                                    setpageSizeVal(pageSize)
-                                  },
-                                  onShowSizeChange: (current, size) => {
-                                    setpageVal(current);
-                                    setpageSizeVal(size)
-                                  },
-                                }
-                              }
-        >
-          <Column title="Order Number" dataIndex="number" key="number"/>
-          <Column title="Status" dataIndex="status" key="status"/>
-          <Column title="OrderTime" dataIndex="orderTime" key="orderTime"/>
-          <Column
-              title="Status"
-              dataIndex="status"
-              key="status"
-              render={(status: number) => (
+        {!isLoading && (
+            <Table
+                dataSource={orderHistoryData}
+                rowKey={(record: DataType) => record.orderTime}
+                onChange={handleTableChange}
+                pagination={{
+                  defaultPageSize: 5,
+                  showSizeChanger: true,
+                  pageSizeOptions: ["5", "10", "20", "30"],
+                  position: ["bottomCenter"],
+                  // onChange={pageSize=>(setPageSize(size))},
+                  total: totalVal,
+                }}
+            >
+              <Column title="Order Number" dataIndex="number" key="number" />
+              <Column title="Status" dataIndex="status" key="status" />
+              <Column title="OrderTime" dataIndex="orderTime" key="orderTime" />
+              <Column
+                  title="Status"
+                  dataIndex="status"
+                  key="status"
+                  render={(status: number) => <span>{statusTextMap[status]}</span>}
+              />
+              <Column title="Price" dataIndex="amount" key="amount" />
+              <Column
+                  title="Action"
+                  key="action"
+                  render={(_: any, record: any) => (
+                      <Space size="middle">
+                        <Link
+                            to={{
+                              pathname: `/orderDetails/${record.id}`,
+                            }}
+                        >
+                          Details{" "}
+                        </Link>
 
-                  <span>{statusTextMap[status]}</span>
-              )}
-          />
-          <Column title="Price" dataIndex="price" key="price"/>
-          <Column
-              title="Action"
-              key="action"
-              render={(_: any, record:any) => (
-                  <Space size="middle">
-
-                    <Link to={{
-                      pathname: `/admin/category/${record.id}`,
-                    }}
-                    >Details</Link>
-
-                    <Popconfirm
-                        title={ 'Are you sure cancel this order?'}
-                        okText="Yes"
-                        cancelText="No"
-                        onConfirm={()=>handleCancel(record.id)}
-                    >
-                      <a>{record.status === 2 ? 'Cancel' : ''}</a>
-                    </Popconfirm>
-
-                  </Space>
-              )}
-          />
-
-        </Table>)}
-
+                        <Popconfirm
+                            title={"Are you sure cancel this order?"}
+                            okText="Yes"
+                            cancelText="No"
+                            onConfirm={() => handleCancel(record.id)}
+                        >
+                          <a>{record.status === 2 ? "Cancel" : ""}</a>
+                        </Popconfirm>
+                      </Space>
+                  )}
+              />
+            </Table>
+        )}
       </div>
-  )
-}
-
+  );
+};
 
 export default OrderHistory;
